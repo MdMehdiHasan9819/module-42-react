0cdffb6ea7f4e5bfadcb6cb2bd6c7c997b2a6cc1
Coder-Mehdi-A2 <mehdihasan9819@gmail.com>
2023-09-14 10:32:56 +0600

    Added all files and first commit

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+    return (
+        <div>
+      <h1 className='text-3xl bg-slate-100 font-extrabold text-blue-500'>Vite + React</h1>
+            
+        </div>
+    );
+};
+
+export default Header;
